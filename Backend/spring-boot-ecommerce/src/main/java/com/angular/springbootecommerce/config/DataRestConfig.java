7f53bce8945fe65adc8199ece477c06c918f6b3e
Jaichyi Chen <jaichyichen@gmail.com>
2023-08-24 13:38:33 -0400

    changed backend configuration to allow read only

---
+++ Backend/spring-boot-ecommerce/src/main/java/com/angular/springbootecommerce/config/DataRestConfig.java
@@ -0,0 +1,31 @@
+package com.angular.springbootecommerce.config;
+
+import com.angular.springbootecommerce.entity.Product;
+import com.angular.springbootecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+        //disable Http Methods for product PUT, POST, and Delete
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)));
+
+        //disable Http Methods for product category PUT, POST, and Delete
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)));
+
+    }
+}
