dd90967e17d35da48d088c4f028bf0b2a55376f2
Jaichyi Chen <jaichyichen@gmail.com>
2023-08-25 14:40:21 -0400

    created service to fetch product list

---
+++ angular-ecommerce-BE/spring-boot-ecommerce/src/main/java/com/angular/springbootecommerce/dao/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.angular.springbootecommerce.dao;
+
+
+import com.angular.springbootecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+
+}
---
+++ angular-ecommerce-FE/src/app/services/product.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl = "http://localhost:8081/api/products"
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<productResponse>(this.baseUrl).pipe(
+      map(data => data._embedded.products)
+    )
+  }
+}
+interface productResponse {
+  _embedded: {
+    products: Product[];
+  }
+}
