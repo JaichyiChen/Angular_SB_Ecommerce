0832dd99b07d31d05aa37c4f1a5a3e327c21788d
Jaichyi Chen <jaichyichen@gmail.com>
2023-08-28 13:20:59 -0400

    changed home page and added styling

--- angular-ecommerce-FE/src/app/services/product.service.ts
+++ angular-ecommerce-FE/src/app/services/product.service.ts
@@ -1,29 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 import { Product } from '../models/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private baseUrl = 'http://localhost:8081/api/products';
 
-  private baseUrl = "http://localhost:8081/api/products"
-
-  constructor(private httpClient: HttpClient) {
-
-  }
-
-
+  constructor(private httpClient: HttpClient) {}
 
   getProductList(): Observable<Product[]> {
-    return this.httpClient.get<productResponse>(this.baseUrl).pipe(
-      map(data => data._embedded.products)
-    )
+    return this.httpClient
+      .get<productResponse>(this.baseUrl)
+      .pipe(map((data) => data._embedded.products));
   }
 }
 interface productResponse {
   _embedded: {
     products: Product[];
-  }
+  };
 }
